eafe81079af7fb8f55f951700ed6abc11fb2cca1
baonguyentran103 <baonguyen.tran103@gmail.com>
2021-11-24 02:38:54 +0700

    complete google login

--- module/passport/login.route.js
+++ module/passport/login.route.js
@@ -6,7 +6,8 @@ const passport = require('./index');
 const userModel = require('../../components/user/user.model');
 const { v4: uuidv4 } = require("uuid");
 const refreshToken =require('../model/refreshToken.model');
-var userProfile;
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client("241758761089-mhhbbvca0eh6nh60sko4td8tp0iqe6r7.apps.googleusercontent.com");
 route.post('/', passport.authenticate('local',  {session:false}), async function(req, res, next){
     console.log(req.user);
     let _rToken = uuidv4();
@@ -19,7 +20,6 @@ route.post('/', passport.authenticate('local',  {session:false}), async function
     }
     await refreshToken.addToken(token);
     res.json({
-        user: req.user,
         token: jwt.sign({
             email: req.user.email,
             id:req.user.id,
@@ -30,61 +30,65 @@ route.post('/', passport.authenticate('local',  {session:false}), async function
         refreshToken: _rToken,
     })
 })
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-  
-  passport.use(new GoogleStrategy({
-    clientID: '241758761089-mhhbbvca0eh6nh60sko4td8tp0iqe6r7.apps.googleusercontent.com',
-    clientSecret: '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',
-    callbackURL: "https://classrom-api-ntk.herokuapp.com/login/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-      userProfile=profile;
-      return done(null, userProfile);
-  }
-));
-route.get('/error', function(req, res){
-    res.json("error logging in");
-  })
-route.get('/auth/google',  passport.authenticate('google', { scope : ['profile', 'email'] }));
-
-route.get('/success', async function(req, res){
-    const user = await userModel.findByMail(userProfile.emails[0].value);
-    if (user === null) {
-        const usergg = {
-            FullName: userProfile.displayName,
-            Email: userProfile.emails[0].value,
+route.post('/google', async function(req, res, next){
+    const tokenId = req.body.tokenId;
+    client.verifyIdToken({idToken: tokenId, audience:"241758761089-mhhbbvca0eh6nh60sko4td8tp0iqe6r7.apps.googleusercontent.com"}).then(async (response) =>{
+        const {email_verified, name, email} = response.payload;
+        console.log(response.payload);
+        if(email_verified){
+            const row = await userModel.findUserByMail(email);
+            if(row == null){
+                var newUser = {
+                    UserID: uuidv4(),
+                    Email: email,
+                    FullName: name,
+                };
+                await userModel.addUser(newUser);
+                let _rToken = uuidv4();
+                let day = new Date();
+                day.setTime(day.getTime() + 60480000)
+                const token ={
+                    Email: email,
+                    Token: _rToken,
+                    ExpiredDate: day,
+                }
+                await refreshToken.addToken(token);
+                console.log("123");
+                res.json({
+                    token: jwt.sign({
+                        email: email,
+                        id: newUser.UserID,
+                        spec: uuidv4(),
+                    }, process.env.JWT_SECRET, {
+                        expiresIn: '5m'
+                    }),
+                    refreshToken: _rToken,
+                })
+            }
+            else{
+                let _rToken = uuidv4();
+                let day = new Date();
+                day.setTime(day.getTime() + 60480000)
+                const token ={
+                    Email: email,
+                    Token: _rToken,
+                    ExpiredDate: day,
+                }
+                await refreshToken.addToken(token);
+                console.log("456");
+                res.json({
+                    token: jwt.sign({
+                        email: email,
+                        id:row[0].UserID,
+                        spec: uuidv4(),
+                    }, process.env.JWT_SECRET, {
+                        expiresIn: '5m'
+                    }),
+                    refreshToken: _rToken,
+                })
+            }
         }
-        await userModel.addUser(usergg);
-        const user_1 = await userModel.findByMail(userProfile.emails[0].value);
-        res.json({
-            user: user_1,
-            token: jwt.sign({
-                email: user_1.Email,
-                id: user_1.UserID,
-            }, process.env.JWT_SECRET, {
-                expiresIn: '1h'
-            })
-        })
         
-    }
-    else{
-        const user_1 = await userModel.findByMail(userProfile.emails[0].value);
-        res.json({
-            user: user_1,
-            token: jwt.sign({
-                email: user_1.Email,
-                id: user_1.UserID,
-            }, process.env.JWT_SECRET, {
-                expiresIn: '1h'
-            })
-        })
-    }
-
-
+    })
 })
-
-route.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/account/error' }), function(req, res) {
-// Successful authentication, redirect success.
-res.redirect('/login/success');
-});
 module.exports = route;
