7245fa0e8f1917f4f4406d644c672935ea0fe3c7
ntdtiendat712 <ntdtiendat712@gmail.com>
2022-01-03 14:07:21 +0700

    minor fix

--- components/gradeClass/gradeClass.model.js
+++ components/gradeClass/gradeClass.model.js
@@ -1,7 +1,7 @@
 const db = require('../../utils/db');
 
 module.exports = {
-    async addGradeInClass(userID, classID, rank, grade){
+    async addGradeInClass(userID, classID, rank, grade) {
         const data = {
             UserID: userID,
             ClassID: classID,
@@ -11,7 +11,7 @@ module.exports = {
         const rs = await db('user_grade').insert(data);
         return rs;
     },
-    async editGradeInClass(userID, classID, rank, grade){
+    async editGradeInClass(userID, classID, rank, grade) {
         const data = {
             UserID: userID,
             ClassID: classID,
@@ -21,17 +21,18 @@ module.exports = {
         const rs = await db('user_grade').where('ClassID', classID).where('UserID', userID).where('Rank', rank).update('Grade', grade);
         return rs;
     },
-    async getGradeOneRankByStudent(userID, classID, rank){
+    async getGradeOneRankByStudent(userID, classID, rank) {
         const rs = await db('user_grade').where('ClassID', classID).where('UserID', userID).where('Rank', rank);
         return rs;
     },
-    async getGradeOfClass(classID){
-        const rs = await db('user_grade as ug').join('gradeStruct as gs', (queryBuilder) =>{
-            queryBuilder.on('ug.ClassID', 'gs.ClassID').andOn('ug.Rank', 'gs.Rank')
-        }).where('ug.ClassID', classID);
+    async getGradeOfClass(classID) {
+        // const rs = await db('user_grade as ug').join('gradestruct as gs', (queryBuilder) => {
+        //     queryBuilder.on('ug.ClassID', 'gs.ClassID').andOn('ug.Rank', 'gs.Rank')
+        // }).where('ug.ClassID', classID);
+        const rs = await db('user_grade as ug').where('ug.ClassID', classID);
         return rs;
     },
-    async getGradeOfStudentInClass(userID, classID){
+    async getGradeOfStudentInClass(userID, classID) {
         const rs = await db('user_grade').where('ClassID', classID).where('UserID', userID);
         return rs;
     }
