db66218c6a8aa3852b523fb0f05fd46d6421e1f3
baonguyentran103 <baonguyen.tran103@gmail.com>
2022-01-20 00:19:28 +0700

    create time of user

--- module/passport/signup.route.js
+++ module/passport/signup.route.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const bcrypt = require('bcryptjs')
 const route = express.Router();
-const userModel = require('../../components/user/user.model');
+const userService = require('../../components/user/user.service');
 const { use } = require('passport');
 route.post('/', async function (req, res, next) {
     console.log(req);
-    const hash = bcrypt.hashSync(req.body.password, 10);
-    var user = {
-        UserID: req.body.id,
-        Password: hash,
-        Email: req.body.email,
-        FullName: req.body.fullName,
-    };
-    console.log(user);
-    await userModel.addUser(user);
+
+    await userService.createUser(req.body);
     res.json('success');
 })
 module.exports = route;
